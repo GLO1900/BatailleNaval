e3077b99319cedd5a3f33f5e5b83856af379ba30
GACHA86 <gabcha25@gmail.com>
2015-11-20 10:47:40 -0500

    Finalisation de la classe Bateau.py

--- Bateau.py
+++ Bateau.py
@@ -4,3 +4,20 @@ class Bateau():
         self.longueur = longueur
         self.coordonnee = coordonnee
 
+    def setOrientation(self, paramorientation):
+        self.orientation = paramorientation
+
+    def getOrientation(self):
+        return self.orientation
+
+    def setLongueur(self, paramlongueur):
+        self.longueur = paramlongueur
+
+    def getlongueur(self):
+        return self.longueur
+
+    def setCoordonnee(self, paramcoordonnee):
+        self.coordonnee = paramcoordonnee
+
+    def getCoordonnee(self):
+        return self.coordonnee
