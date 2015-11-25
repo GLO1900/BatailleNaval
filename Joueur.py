d404c1970dfa10b72da09bcebe9f4515e4f19d46
GACHA86 <gabcha25@gmail.com>
2015-11-25 12:49:39 -0500

    -Construction de la grille sur l'interface graphique dans Vuegrille.py -Correction de l'indentation de la classe Joueur.py

--- Joueur.py
+++ Joueur.py
@@ -1,9 +1,11 @@
 from Bateau import Bateau
 
+
 class Joueur():
     def __init__(self, nom):
-        self.listeBateau = [Bateau(0,2,(0,0),'Torpilleur'),Bateau(0,3,(0,0),'Contre-Torpilleur')]
+        self.listeBateau = [Bateau(0, 2, (0, 0), 'Torpilleur'), Bateau(0, 3, (0, 0), 'Contre-Torpilleur')]
         self.nom = nom
+
     def __str__(self):
         string = ''
         string += self.nom
--- Vuegrille.py
+++ Vuegrille.py
@@ -1,19 +1,48 @@
 import Grille
 import turtle
 
-
 DEFAULT_SCREEN_WIDTH = 800
 DEFAULT_SCREEN_HEIGHT = 600
+
+
 class Vuegrille:
     def __init__(self):
         self.screen = turtle.Screen()
         self.screen.title("Bataille Navale")
+
         self.setScreenSize(DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT)
-
-
+        self.setGrille()
         turtle.done()
 
     def setScreenSize(self, largeur, hauteur):
-        self.screen.setup(largeur, hauteur)
+        self.screen.setup(DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT, None, None)
+        self.screen.bgcolor('blue')
+
+    def setGrille(self):
+        margin = (DEFAULT_SCREEN_WIDTH // 20)
+        print(margin)
+        animationGrille = turtle.Turtle()
+        animationGrille.penup()
+        animationGrille.setx(-DEFAULT_SCREEN_WIDTH/2 + margin)
+        animationGrille.sety(DEFAULT_SCREEN_HEIGHT/2 - margin)
+        animationGrille.right(90)
+
+        for i in range(Grille.DEFAULT_SIZE + 1):
+            animationGrille.pendown()
+            animationGrille.forward(margin * Grille.DEFAULT_SIZE)
+            animationGrille.penup()
+            animationGrille.sety(DEFAULT_SCREEN_HEIGHT/2 - margin)
+            animationGrille.setx(animationGrille.xcor() + margin)
+
+        animationGrille.setx(-DEFAULT_SCREEN_WIDTH/2 + margin)
+        animationGrille.sety(DEFAULT_SCREEN_HEIGHT/2 - margin)
+        animationGrille.left(90)
+        for i in range(Grille.DEFAULT_SIZE + 1):
+            animationGrille.pendown()
+            animationGrille.forward(margin * Grille.DEFAULT_SIZE)
+            animationGrille.penup()
+            animationGrille.sety(animationGrille.ycor() - margin)
+            animationGrille.setx(-DEFAULT_SCREEN_WIDTH/2 + margin)
+
 
 Vuegrille()
